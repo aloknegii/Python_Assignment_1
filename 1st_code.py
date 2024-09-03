35d7db344cfcadc91d1e31924b32d06a130009ed
aloknegii <aloknegii147@gmail.com>
2024-09-03 14:08:31 +0530

    Add files via upload

---
+++ 1st_code.py
@@ -0,0 +1,10 @@
+import sys # Import sys module to access system-specific parameters and functions 
+ 
+# Print student's name
+print("Name: Alok Negi")
+
+# Print student's course name
+print("Course Name: MCA")
+
+# Print the version name of Python that is being used
+print("Python Version:", sys.version)
